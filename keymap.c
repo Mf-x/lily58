ee054cdeecc1ce3e880cb9a5e19a62449159e99d
Mf-x <106863923+Mf-x@users.noreply.github.com>
2022-06-06 21:09:28 +0200

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -139,3 +139,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
+
+#ifdef ENCODER_ENABLE
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
+        }
+    }
+    return true;
+}
+#endif
+
